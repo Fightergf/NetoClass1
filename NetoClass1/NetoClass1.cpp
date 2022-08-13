8b0eccf384aca65eb8a65584427507152ca6d2ed
DenisMir <pete4kob12@mail.ru>
2022-08-13 10:13:46 +0300

    work3

--- NetoClass1/NetoClass1.cpp
+++ NetoClass1/NetoClass1.cpp
@@ -37,49 +37,71 @@ public:
     }
     bool set_num1(double num1)
     {
-        while (num1 == 0)
-        {
-            if (num1 == 0)
-            {
-                std::cout << "Неверное значение!!" << "\n";
-            }
-            std::cout << "Введите num1: \n";
-            std::cin >> num1;
-        }
         return this->num1 = num1;
     }
     bool set_num2(double num2)
     {
-        while (num2 == 0)
-        {
-            if (num2 == 0)
-            {
-                std::cout << "Неверное значение!!" << "\n";
-            }
-            std::cout << "Введите num2: \n";
-            std::cin >> num2;
-        }
         return this->num2 = num2;
     }
 };
 
-int main()
+void setValue1(Calculator &calc)
 {
-    setlocale(LC_ALL, "Ru");
-    double num1, num2;
+    double num1;
+    bool i = true;
+    while (i)
+    {
+        std::cout << "Введите num1: \n";
+        std::cin >> num1;
+        if (num1 == 0)
+        {
+            std::cout << "Неверное значение!!" << "\n";
+        }
+        else
+        {
+            calc.set_num1(num1);
+            i = false;
+        }
+    }
+}
+void setValue2(Calculator& calc)
+{
+    double num2;
+    bool i = true;
+    while (i)
+    {
+        std::cout << "Введите num2: \n";
+        std::cin >> num2;
+        if (num2 == 0)
+        {
+            std::cout << "Неверное значение!!" << "\n";
+        }
+        else
+        {
+            calc.set_num2(num2);
+            i = false;
+        }
+    }
+}
 
-    Calculator calc;
-    std::cout << "Введите num1: \n";
-    std::cin >> num1;
-    std::cout << "Введите num2: \n";
-    std::cin >> num2;
-
-    calc.set_num1(num1);
-    calc.set_num2(num2);
+void printResult(Calculator &calc)
+{
     std::cout << "num1 + num2 = " << calc.add() << "\n";
     std::cout << "num1 * num2 = " << calc.multiply() << "\n";
     std::cout << "num2 - num1 = " << calc.subtract_1_2() << "\n";
     std::cout << "num1 - num2 = " << calc.subtract_2_1() << "\n";
     std::cout << "num1 / num2 = " << calc.divide_1_2() << "\n";
     std::cout << "num2 / num1 = " << calc.divide_2_1() << "\n";
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    Calculator calc;
+    
+    setValue1(calc);
+    setValue2(calc);
+
+    printResult(calc);
 }
